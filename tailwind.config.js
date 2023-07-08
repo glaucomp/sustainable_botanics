3bdcd25162ddc95bdd18fd567c098195a0425d7c
Glauco Pereira <glaucomp@hotmail.com>
2023-07-08 16:05:07 +1000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  './App.{js,jsx,ts,tsx}", "./<custom-folder>/**/*.{js,jsx,ts,tsx}',
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
